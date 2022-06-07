2046574e464a827fb2c7e958e46d3384495b962e
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-06-08 07:09:47 +0800

    update on routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import Accounts from "./routes/Accounts";
+import axios from 'axios';
 
 function App() {
-  return <ThemeProvider>
+  const [coins, setCoins] = useState([])
+
+  const url = 
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+
+  useEffect(()=> {
+    axios.get(url).then((response)=> {
+      setCoins(response.data)
+      console.log(response.data)
+    })
+  }, [url])
+
+  return (
+  <ThemeProvider>
     <Navbar />
-  </ThemeProvider>;
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/accounts' element={<Accounts />} />
+    </Routes>
+  </ThemeProvider>
+  )
 
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ThemeToggle } from './ThemeToggle'
-import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -24,12 +24,15 @@ const Navbar = () => {
         </div>
 
         {/* Menu Icon*/}
-        <div className='block md:hidden cursor-pointer z-10'>
-            <AiOutlineMenu />
+        <div onClick={handleNav} className='block md:hidden cursor-pointer z-10'>
+            {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
 
         {/* Mobile Menu */}
-        <div className='md:hidden fixed left-0 top-20 flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10'>
+        <div 
+            className={
+                nav ? 'md:hidden fixed left-0 top-20 flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10' 
+                : 'fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-300'}>
             <ul className='w-full p-4'>
                 <li className='border-b py-6'>
                     <Link to='/'>Home</Link>
