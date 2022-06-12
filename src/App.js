5182544018817cd2bd94610bd31e46121b48f1bd
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-06-12 18:42:30 +0800

    update components and context

--- src/App.js
+++ src/App.js
@@ -6,40 +6,42 @@ import Home from "./routes/Home";
 import Signin from "./routes/Signin";
 import Signup from "./routes/Signup";
 import Account from "./routes/Account";
-import axios from 'axios';
-import CoinPage from './routes/CoinPage'
+import axios from "axios";
+import CoinPage from "./routes/CoinPage";
 import Footer from "./components/Footer";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
 
-  const url = 
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
 
-  useEffect(()=> {
-    axios.get(url).then((response)=> {
-      setCoins(response.data)
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
       // console.log(response.data)
-    })
-  }, [url])
+    });
+  }, [url]);
 
   return (
-  <ThemeProvider>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home coins={coins} />} />
-      <Route path='/signin' element={<Signin />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/account' element={<Account />} />
-      <Route path='/coin/:coinId' element={<CoinPage />}>
-          <Route path=':coinId' />
-        <Route />
-      </Route>
-    </Routes>
-    <Footer />
-  </ThemeProvider>
-  )
-
+    <ThemeProvider>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
+            <Route />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
